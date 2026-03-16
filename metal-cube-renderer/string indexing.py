6082f12c6d20247a415467c6fbf0bc8d051c6853
cue <onetapfact@gmail.com>
2026-03-16 16:54:16 +0530

    indexing

--- metal-cube-renderer/string indexing.py
+++ metal-cube-renderer/string indexing.py
@@ -1,1 +1,10 @@
-phone = int(input("enter your mobile number : "))
+phone = "91-1234-5678"
+a = phone[0:2]
+b = phone[3:]
+c = phone[::-1]
+d = phone[0::2]
+
+print(f"your regional number is {a}-xxx-xxx ")
+print(f"your main number is {b}")
+print(f"your reveresd number is {c}")
+print(f"your number with 2 gaps is {d}")
