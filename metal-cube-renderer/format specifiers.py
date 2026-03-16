6bad2eeec251c9afc5953395cf8167c17526d80b
cue <onetapfact@gmail.com>
2026-03-16 17:25:52 +0530

    specifiers

---
+++ metal-cube-renderer/format specifiers.py
@@ -0,0 +1,19 @@
+price = 109.81
+price2 = 78.98
+price3 = -78234697
+
+print(f"price of 1st item is {price:>10,.2f}")
+print(f"price of 2st item is {price2:>10,.2f}")
+print(f"price of 3st item is {price3:>10,.2f}")
+
+
+# .(number)f = round to that many decimal places (fixed point)
+#: (number) = allocate that many spaces
+# :03 = allocate and zero pad that many spaces
+# :< = left justify
+# : = right justify
+# :^ = center align
+# :+ = use a plus sign to indicate positive value
+# := = place sign to leftmost position
+# : = insert a space before positive numbers
+# :, = comma separator
