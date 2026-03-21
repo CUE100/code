ccf93273952dd1620b65c016f7e6fc13d3c43b72
cue <onetapfact@gmail.com>
2026-03-21 21:07:30 +0530

    hi

---
+++ python learning/lists,sets,tuples.py
@@ -0,0 +1,32 @@
+# cars = ["mercedes", "BMW", "audi", "porsche911"]
+
+# for car in cars:
+# print(dir(cars))
+# print(len(cars))
+# print("toyota" in cars)
+# cars[0] = "toyota".  # to change one value to another
+# print("toyota" in cars)
+# print(cars)
+# cars.remove("BMW")
+# cars.insert(2, "mclaren ")
+# cars.sort()
+# cars.reverse()
+# cars.clear()
+# print(cars.index("BMW"))
+# print(cars)
+
+
+# cars = {"mercedes", "BMW", "audi", "porsche911"}
+# print(dir(cars))
+# print(len(cars))
+# cars.add("alto")
+# cars.remove("BMW")
+# cars.clear()
+# print(cars)
+
+cars = ("mercedes", "BMW", "audi", "porsche911")
+# print(dir(cars))
+# print(help(cars))
+# print(len(cars))
+# print("audi" in cars)
+print(cars)
---
+++ python learning/small test.py
@@ -0,0 +1,15 @@
+# a = int(input("enter the number of rows : "))
+# b = int(input("enter the number of columns : "))
+# c = input("enter the symbol to use: ")
+# for x in range(a):
+#   for y in range(b):
+#       print(c, end="")
+#    print()
+
+
+a = int(input("enter the number : "))
+
+
+for x in range(a):
+    for y in range():
+        print()
