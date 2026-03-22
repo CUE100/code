bf92db64f6d7fe6bd9b016ebc3fd197df405f95e
cue <onetapfact@gmail.com>
2026-03-22 14:44:40 +0530

    hi

--- python learning/countdown timer programme.py
+++ python learning/countdown timer programme.py
@@ -5,7 +5,7 @@ for x in reversed(range(1, a)):
     time.sleep(1)
     seconds = x % 60
     minutes = int(x / 60) % 60
-    hours = (x // 60) % 24
+    hours = int(((x / 60)/60) % 24)
     days = x // (24 * 60 * 60)
     print(f"{days:02}:{hours:02}:{minutes:02}:{seconds:02}")
 
