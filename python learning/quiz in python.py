1a7c81da0f99134e5d100dcdd41c8ebf43cc40fe
cue <onetapfact@gmail.com>
2026-03-27 12:46:56 +0530

    changes

--- python learning/quiz in python.py
+++ python learning/quiz in python.py
@@ -1,17 +1,54 @@
 questions = (("What's color of water? "), ("How many planet in solar system? "),
-             ("how is dakshwaligame ? "), ("what is name of our galaxy? "), ("why school is worst for learning? "))
+             ("How many elements in our periodic table?  "), ("What is name of our galaxy? "), ("How many bones do humans have? "))
 
-options = (("A.Green ", "B.Blue", "C.Red", "D.Black"), ("A.6", "B.7", "C.8", "D.9"), ("A.Worst", "B.Neutral", "C.Good", "D.Best"), ("A.Milky way",
-           "B.Alpha centuri", "C.Andrmeda galaxy", "D.Nether"), ("A.Because it doesnot teach skill", "B.Because its not valuable", "C.I oppose it", "D.IDK"))
+options = (("A.Green ", "B.Blue", "C.Red", "D.Black"), ("A.6", "B.7", "C.8", "D.9"), ("A.118", "B.117", "C.112", "D.119"), ("A.Milky way",
+           "B.Alpha centuri", "C.Andrmeda galaxy", "D.Nether"), ("A.206", "B.306", "C.406", "D.IDK"))
 
 correct_answers = ("B", "C", "A",
-                   "A", "B")
+                   "A", "A")
 
 users_guess = []
-
+score = 0
 question_num = 0
 
-for x in questions:
-    print(x)
-    print()
-    users_answer = input("enter your answer (A,B,C,D)")
+
+for question in questions:
+    print("--------------------------")
+    print("--------------------------")
+    print(question)
+
+    for option in options[question_num]:
+        print(option)
+
+    print("--------------------------")
+
+    users_answer = input("enter your answer (A,B,C,D): ").upper()
+    users_guess.append(users_answer)
+    print("")
+
+    if users_answer == correct_answers[question_num]:
+        print("Correct !!")
+        score += 1
+    else:
+        print("Incorrect !! ")
+        print(f"The correct answer is {correct_answers[question_num]}")
+    question_num = question_num + 1
+
+
+print("--------------------------")
+print("        RESULT            ")
+print("--------------------------")
+
+
+print("your guesses: ", end=" ")
+for guess in users_guess:
+    print(guess, end=" ")
+print()
+print("Correct answer: ", end=" ")
+for answer in correct_answers:
+    print(answer, end=" ")
+
+score = score / question_num * 100
+print()
+print(f"Your final score is {score} % .")
+print()
