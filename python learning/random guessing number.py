95431b2fd031e6a7ac2b4de2b86cda09412adcbe
cue <onetapfact@gmail.com>
2026-03-28 22:56:39 +0530

    changes

---
+++ python learning/random guessing number.py
@@ -0,0 +1,35 @@
+import random
+
+print("-----------------------------------------------------")
+
+rand_num = random.randint(1, 100)
+user_num = int(input("enter a number between 1 and 100 : "))
+attempts = 0
+
+while user_num not in range(1, 100):
+    user_num = int(input("enter a number between 1 and 100 : "))
+    if user_num in range(1, 100):
+        break
+
+while True:
+
+    if user_num > rand_num:
+        print("-----------------------------------------------------")
+        print("random number is less!")
+        user_num = int(input("enter a number between 1 and 100 : "))
+        attempts += 1
+    elif user_num < rand_num:
+        print("-----------------------------------------------------")
+        print("random number is greater!")
+        user_num = int(input("enter a number between 1 and 100 : "))
+        attempts += 1
+
+    if user_num == rand_num:
+        print("-----------------------------------------------------")
+        print("you have identified correct number!")
+        attempts += 1
+        break
+
+print("-----------------------------------------------------")
+print(f"You took {attempts} attempts to guess random number! ")
+print("-----------------------------------------------------")
---
+++ python learning/rock,paper,scissor .py
@@ -0,0 +1,36 @@
+import random
+import time
+
+options = ["rock", "paper", "scissors"]
+print("--- Welcome to Rock, Paper, Scissors! ---")
+
+
+while True:
+    bot_choice = random.choice(options)
+    user_choice = input(
+        "\nEnter rock, paper, or scissors (or 'q' to quit): ").lower().strip()
+
+    if user_choice == 'q':
+        print("Thanks for playing!")
+        break
+
+    if user_choice not in options:
+        print("Invalid input. Please try again.")
+        continue
+    print("Bot result in few seconds...")
+    for x in reversed(range(4)):
+        time.sleep(1)
+        print(x)
+
+    print(f"Bot chose: {bot_choice}")
+
+    if user_choice == bot_choice:
+        print(f"Both chose {user_choice}. It's a tie!")
+
+    elif (user_choice == "rock" and bot_choice == "scissors") or \
+         (user_choice == "paper" and bot_choice == "rock") or \
+         (user_choice == "scissors" and bot_choice == "paper"):
+        print("You won against the bot!")
+
+    else:
+        print("You lost against the bot!")
