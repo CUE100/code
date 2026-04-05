83f5b65c5f058b1a773c8a06ffbd99424fc075b0
cue <onetapfact@gmail.com>
2026-04-05 17:18:07 +0530

    list comprehension

---
+++ python learning/list comprehension.py
@@ -0,0 +1,22 @@
+# expression for value in iterables if condition
+
+# cars = ["ferrari", "lambo", "supra", "aston martin"]
+# cars = [y[0] for y in cars]
+# print(cars)
+
+# cars = ["ferrari", "lambo", "supra", "aston martin"]
+# cars = [y[0] for y in cars]
+# print(cars)
+
+# cars = ["ferrari", "lambo", "supra", "aston martin"]
+# cars = [y[0] for y in cars if 2 + 2 == 4]
+# print(cars)
+
+# cars = [["urus", "gwagon", "sclass", "defender"], ["ferrari", "lambo",
+# "porsche", "aston martin"], ["tesla", "electric porsche"]]
+# user_input = int(
+# input("Enter a number : 1 for suv ,  2 for sports , 3 for EV: "))
+# cars = [cars[0] for y in cars if user_input == 1]
+# cars = [cars[1] for y in cars if user_input == 2]
+# cars = [cars[2] for y in cars if user_input == 3]
+# print(cars)
