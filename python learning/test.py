5f209f6d85aeee44be84833fa00b4c061c73b45c
cue <onetapfact@gmail.com>
2026-04-06 20:48:48 +0530

    changes

--- python learning/test.py
+++ python learning/test.py
@@ -1,46 +1,41 @@
 import time
-print("")
+print("------------------------------------------------------")
 print("WELCOME TO OUR CAFE !!!")
 
-item = input("enter the item you want to buy : ")
-quantity = int(input("enter the quantity of item you want to buy : "))
-price = float(input("enter the price of item : "))
+
+def details():
+    item = input("Enter the item you want to buy : ")
+    quantity = int(input("Enter the quantity of item you want to buy : "))
+    price = float(input("Enter the price of item : "))
+    return item, quantity, price
 
 
-if not item.isalpha():
-    print("your item should not contain numbers !! ")
+def calculation(quantity, price):
+    total = quantity * price
+    is_member = input(
+        "Are you memeber of our cafe ? (y/n) : ").lower().replace(".", "").replace(".", "")
+    age = int(input("enter your age : "))
+    if is_member == "y" and age >= 60:
+        total = total * 0.80
+    elif is_member == "y" and age < 60:
+        total = total * 0.90
+    return total, age, quantity, price
 
-else:
-    print("heres your full details : ")
+
+def reciept(item, quantity, total):
+    print("your food in 3 seconds ")
+    for x in range(0, 3):
+        time.sleep(1)
+        print(x)
+
+    print("Heres your food: ")
+    print(
+        f"You bought {item.capitalize()} ,{quantity} times, with your subtotal of {round(total, 2)} $ !! ")
+    print("Have a nice day !!")
+
+# MAIN PROGRAMME SKELTON
 
 
-total = quantity * price
-
-is_member = (input("are you memeber of our cafe ? (y/n) : ").lower()
-             ).replace(".", "").replace(".", "")
-age = int(input("enter your age : "))
-
-if is_member == "y" and age >= 60:
-    total = total * 0.80
-    print("as you are above 60 y and are member , you get 20 percent discount ")
-    print(f"so your subtotal is {round(total, 2)} ")
-
-elif is_member == "y" and age < 60:
-    total = total * 0.90
-    print("as you are below 60 y and are member , you get 10 percent discount ")
-    print(f"so your subtotal is {round(total, 2)}")
-
-else:
-    print("you have to pay full price :( ")
-
-status = "VIP user as subtotal greater than 50 $" if total >= 50 else "standard user as subtotal less than 50 $"
-print(status)
-
-for x in range(0, 3):
-    time.sleep(1)
-    print(x)
-print("your food in 3 seconds ")
-print("heres your food ")
-print(
-    f"you bought {item.capitalize()} ,{quantity} times, with your subtotal of {round(total, 2)} $ !! ")
-print("have a nice day !!")
+item, quantity, price = details()
+total, age, quantity, price = calculation(quantity, price)
+reciept(item, quantity, total)
