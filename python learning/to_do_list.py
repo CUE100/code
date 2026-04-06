deb75a394229c34dcd7ba15f2b345d7a9a4ddfb9
cue <onetapfact@gmail.com>
2026-04-06 19:47:45 +0530

    changes

--- python learning/to_do_list.py
+++ python learning/to_do_list.py
@@ -1,10 +1,10 @@
 work_today = []
-is_running = True
 
 print("-------------------------------------------------")
 print("------------------TO DO LIST APP-----------------")
 
-while is_running:
+
+def to_do():
     print("-------------------------------------------------")
     print("What you want to do? ")
     print("1.Add a task to today's to-do list.(Press 1) ")
@@ -12,51 +12,59 @@ while is_running:
     print("3.Name the task you want to remove.(Press 3)")
     print("4.Quit this app.(Press 4) ")
     print()
+
+
+def add_task():
+    print("-------------------------------------------------")
+    while True:
+        user_add = input(
+            "Enter an work you want to add(press q to main menu): ").upper()
+        if user_add == "Q":
+            break
+        work_today.append(user_add)
+
+
+def show_task():
+    n = 0
+    print("------------------YOUR TO DO--------------------")
+    for work in work_today:
+        n += 1
+        print(f"{n}.{work}")
+
+
+def remove_task_function():
+    print("-------------------------------------------------")
+    while True:
+        m = 0
+        remove_task = input(
+            "Name the task you want to remove(e to end and return to main menu): ").upper()
+        if remove_task == "E":
+            break
+        if remove_task not in work_today:
+            print("Please enter a task that is already in your today's work!")
+        else:
+            show_task()
+
+
+def exit_function():
+    print("-------------------------------------------------")
+    print("Thanks for using this app! ")
+
+
+is_running = True
+while is_running:
+    to_do()
     try:
         user_number = input("Enter your choice: ")
         user_number = int(user_number)
     except ValueError:
         print("Please enter a valid choice!")
-        continue
-
     if user_number == 1:
-        print("-------------------------------------------------")
-        while True:
-            user_add = input(
-                "Enter an work you want to add(press q to main menu): ").upper()
-            work_today.append(user_add)
-            if user_add == "Q":
-                work_today.remove("Q")
-                break
-
+        add_task()
     elif user_number == 2:
-        n = 0
-        print("------------------YOUR TO DO--------------------")
-        for work in work_today:
-            n += 1
-            print(f"{n}.{work}")
-
+        show_task()
     elif user_number == 3:
-
-        print("-------------------------------------------------")
-        while True:
-            m = 0
-            remove_task = input(
-                "Name the task you want to remove(e to end and return to main menu): ").upper()
-            if remove_task == "E":
-                break
-            if remove_task not in work_today:
-                print("Please enter a task that is already in your today's work!")
-
-            else:
-                print("-------------------------------------------------------")
-                work_today.remove(remove_task)
-                print("------------------------UPDATED LIST-------------------")
-                for work in work_today:
-                    m += 1
-                    print(f"{m}.{work}")
-
+        remove_task_function()
     elif user_number == 4:
-        print("-------------------------------------------------")
-        print("Thanks for using this app! ")
-        break
+        exit_function()
+        is_running = False
