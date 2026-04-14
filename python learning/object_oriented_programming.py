3d7fe414569660b03b75f41b5b6a50889b9f53a9
cue <onetapfact@gmail.com>
2026-04-14 15:06:41 +0530

    oop

--- python learning/object_oriented_programming.py
+++ python learning/object_oriented_programming.py
@@ -1,1 +1,20 @@
 # ONE OF THE MOST IMPORTANT TOPICS
+class Car:
+    def __init__(self, model, year, color, engine):
+        self.model = model
+        self.year = year
+        self.color = color
+        self.engine = engine
+
+    def details(self):
+        print(
+            f"You have {self.year} - {self.model} with {self.engine} of {self.color} color")
+
+    def electric_car(self, battery_size):
+        print(
+            f"You have {self.year} - {self.model} with {self.engine} of {self.color} color and has {battery_size}kg batter weight.")
+
+
+car1 = Car("Porsche911", "2026", "Aesthetic white-grey", "12CC(V12 engine)")
+car2 = Car("Tesla", 2026, "white", "V6")
+car2.electric_car(12)
