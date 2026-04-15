625713df260417801b0da1eaea38322ef6b79497
cue <onetapfact@gmail.com>
2026-04-15 20:47:47 +0530

    a

--- python learning/project.py
+++ python learning/project.py
@@ -1,1 +1,16 @@
+class Car:
+    def turn_on(self):
+        print("You start the engine.")
+        return self
 
+    def drive(self):
+        print("You step on the gas.")
+        return self  # Returns the object itself
+
+    def brake(self):
+        print("You step on the brakes.")
+
+
+my_car = Car()
+# Because each method returns 'self', you can chain them together:
+my_car.turn_on().drive().brake()
