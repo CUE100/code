06612a3412eed803b6d80e26e137d239df8c8e66
cue <onetapfact@gmail.com>
2026-04-25 19:17:38 +0530

    #

---
+++ python learning/class_methods.py
@@ -0,0 +1,23 @@
+class Students:
+    total_gpa = 0
+    count = 0
+
+    def __init__(self, name, gpa):
+        self.name = name
+        self.gpa = gpa
+        Students.count += 1
+        Students.total_gpa += gpa
+
+    def get_infpo(self):
+        return f"The name of student is {self.name} with {self.gpa}"
+
+    @classmethod
+    def avg_gpa(cls):
+        return f"Average gpa of student is {Students.total_gpa / Students.count:.2f}"
+
+
+a = Students("A", 9)
+b = Students("B", 3.8)
+c = Students("C", 3.8)
+
+print(Students.avg_gpa())
