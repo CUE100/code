d0b7130da2c02d91d57631087480acb739927b2f
cue <onetapfact@gmail.com>
2026-05-02 18:48:27 +0530

    changes in magic methods

--- python learning/magic_methods.py
+++ python learning/magic_methods.py
@@ -1,1 +1,33 @@
- 
+# magic methods
+class Book:
+
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __lt__(self, other):
+        return self.pages < other.pages
+
+    def __gt__(self, other):
+        return self.pages > other.pages
+
+    def __eq__(self, other):
+        return self.author == other.author and self.title == other.title
+
+    def __add__(self, other):
+        return self.pages + other.pages
+
+    def __contains__(self, keyword):
+        return keyword in self.title or keyword in self.author
+
+    def __getitem__(self, key):
+        if key == "title":
+            return self.title
+        elif key == "author":
+            return self.author
+
+
+book1 = Book("ATOMIC HABITS", "JAMES CLEAR", 300)
+book2 = Book("HYPER FOCUS", "CHRIS BAILEY", 200)
+print(book1 == book2)
---
+++ python learning/property.py
@@ -0,0 +1,16 @@
+class Car:
+
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def car_brand(self):
+        pass
+
+    def car_model(self):
+        pass
+
+
+car = Car("Porsche", "911")
+print(car.brand)
+print(car.model)
