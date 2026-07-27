7de5ff7f0fe0532dcc4901c655074bed302045d3
cue <onetapfact@gmail.com>
2026-07-27 18:08:18 +0530

    chnages

--- pytorch/build_a_model/datasets_x_tensors_x_model.py
+++ pytorch/build_a_model/datasets_x_tensors_x_model.py
@@ -1,8 +1,7 @@
-import torch 
 from torch import nn
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets
+from torchvision.transforms import ToTensor
 
 labels_map = {
     0: "T-shirt/top",
@@ -17,23 +16,51 @@ labels_map = {
     9: "Ankle boot",
 }
 
+
 class NameDataset(Dataset):
     def __init__(self):
         self.image = datasets.FashionMNIST(
-            root="data", download=True , train=False
+            root="data", download=True, train=False, transform=ToTensor()
         )
+
     def __len__(self):
         return len(self.image)
-    
-    def __getitem__(self,idx):
-        image , label = self.image[idx]
-        return image,label
-    
-    def labelname(self,label,labels_map):
-        label_name = labels_map[label]
-        return label_name,label
-    
+
+    def __getitem__(self, idx):
+        imagee, label = self.image[idx]
+        return imagee, label
+
+    def labelname(self, label, labels_map):
+        return labels_map[label]
 
 
-    
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
 
+        self.flatten = nn.Flatten()
+
+        self.nn_sequential = nn.Sequential(
+            nn.Linear(28*28, 784),
+            nn.ReLU(),
+            nn.Linear(784, 20),
+            nn.ReLU(),
+            nn.Linear(20, 10)
+        )
+
+    def forward(self, original_image):
+
+        x = self.flatten(original_image)
+        x = self.nn_sequential(x)
+
+        return x
+
+
+ds = NameDataset()
+
+loader = DataLoader(ds, batch_size=64, shuffle=True)
+model = NeuralNetwork()
+
+images, labels = next(iter(loader))
+logits = model(images)
+print(logits.shape)
